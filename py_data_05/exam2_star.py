1017f692a175a07c206dff9320716a99f4155ba4
nailoc23 <nailoc23@naver.com>
2023-07-21 17:07:23 +0900

    수정 07-21

--- py_data_05/exam2_star.py
+++ py_data_05/exam2_star.py
@@ -20,32 +20,34 @@ store_btn = driver.find_element(By.CSS_SELECTOR, 'div.store_bn1_btn>a')
 store_btn.click()
 
 #페이지 이동됨
-URL2 = 'https://www.starbucks.co.kr/store/store_map.do'
-print(URL2)
+URL2 = driver.current_url
 driver.quit()
 driver2 = webdriver.Chrome()
 driver2.get(URL2)
 
-time.sleep(3)
 local_taB = driver2.find_element(By.CSS_SELECTOR, 'header.loca_search a')
 local_taB.click() #지역검색
+time.sleep(2)
 
-time.sleep(3)
 sidoes = driver2.find_element(By.CLASS_NAME, 'sido_arae_box')
 seoul_li = sidoes.find_elements(By.TAG_NAME, 'li')
 seoul_li[0].click() # 시도 (서울)
+time.sleep(2)
 
-time.sleep(3)
 gugun = driver2.find_element(By.CLASS_NAME, 'gugun_arae_box')
 yongdungpo_li = gugun.find_elements(By.TAG_NAME, 'li')
 yongdungpo_li[20].click() # 데이터를 가져오는데 시간이 걸림
-time.sleep(3)
+time.sleep(2) # 비동기로 데이터를 가져오기 때문에 명시적인 일시정지가 필요함
 
 #크롤링 시작
 html_doc = driver2.page_source
 soup = BeautifulSoup(html_doc, 'html.parser')
 quickresult = soup.find('ul', class_='quickSearchResultBoxSidoGugun')
 starbuck_stores = quickresult.find_all('li')
-print('갯수:', len(starbuck_stores))
+print('영등포 매장갯수:', len(starbuck_stores))
+for store in starbuck_stores:
+    print('매장명:', store.find('strong').text)
+    print('매장주소:', store.find('p').text)
+    print()
 
 time.sleep(10)
