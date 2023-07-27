08be97366a1039845e78d449a9c997f64b205145
nailoc23 <nailoc23@naver.com>
2023-07-27 17:40:38 +0900

    AI 활용 파이썬 개발 추가

--- py_data_07/exam2.py
+++ py_data_07/exam2.py
@@ -1,5 +1,12 @@
 import numpy as np
 import pandas as pd
+
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_path ="C:/Windows/Fonts/NANUMMYEONGJO.TTF"
+font = font_manager.FontProperties(fname=font_path).get_name()
+rc('font', family=font)
+
 # 1번: 서울에 있는 치킨집 xlsx 데이터를 읽어서 출력
 xlsx_filename = 'data/치킨집_가공.xlsx'
 df = pd.read_excel(xlsx_filename)
@@ -37,4 +44,29 @@ result = pd.DataFrame(dong_count, columns=['chk_cnt'], index=uni_dong)
 df2 = pd.DataFrame(dong, columns=['dong'])
 df = pd.concat([df, df2], axis=1)
 df_group_dong = df.groupby('dong')['소재지전체주소'].count()
-print(df_group_dong)
+dong_sorted = list(df_group_dong.index)
+
+#print(df_group_dong) # 시리즈
+
+# 동별로 치킨집의 갯수를 bar 그래프와
+# 비율을 pie 그래프로 나타내시오
+x_pos = np.arange(len(uni_dong))
+y_count = df_group_dong.to_list()
+
+# 2개의 서브 plt
+fig = plt.figure()
+sub_plt1 = fig.add_subplot(2,1,1) # 행2 열1 수1
+sub_plt1.set_xticklabels(dong_sorted, fontsize=8)
+sub_plt2 = fig.add_subplot(2,1,2) # 행2 열1 수1
+
+
+plt.subplot(2,1,1)
+plt.bar(x_pos, y_count)
+plt.xticks(x_pos, dong_sorted)
+
+plt.subplot(2,1,2)
+plt.pie(y_count, labels=dong_sorted, autopct='%.1f%%', textprops={'fontsize':6})
+
+plt.show()
+
+
